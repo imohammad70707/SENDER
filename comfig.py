64fbc7d943e7a314e6ab9c0252e0f8986bf0788f
imohammad70707 <mogh.sma@gmail.com>
2026-09-10 04:38:04 +0330

    Create comfig.py

---
+++ comfig.py
@@ -0,0 +1,27 @@
+BOT_TOKEN = "CDFCJA0GRKANOPQXFENKFNARXQHUPOBTPNSWXEIYPGQQNLCCJIVNRWTGSXOMKQOA"
+
+GROUP_ID = "g0IUUyj08575f118c1c329134c1e7c5a"
+
+
+# --------------------------------------------------
+# پوشه‌هایی که برنامه مجاز به بررسی آن‌هاست
+# --------------------------------------------------
+
+SOURCE_FOLDERS = [
+    "/storage/emulated/0/DCIM",
+    "/storage/emulated/0/Pictures",
+    "/storage/emulated/0/Movies",
+    "/storage/emulated/0/Music",
+    "/storage/emulated/0/Download",
+]
+
+
+# --------------------------------------------------
+# حداکثر تعداد فایل برای قرار گرفتن در ZIP
+# --------------------------------------------------
+
+# حداکثر تعداد فایل
+MAX_FILES = 50
+
+# حداکثر حجم مجموع فایل‌ها
+MAX_TOTAL_SIZE_MB = 50
